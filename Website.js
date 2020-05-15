8a35af8c505f6e185648b26989c78e1beb364c4c
asamra18 <anandsamra@hotmail.com>
2020-05-14 22:51:10 -0700

    Have core functionality done. Need to make it pretty and organize buttons and colors.

---
+++ Website.js
@@ -0,0 +1,16 @@
+function revealFrom(){
+    document.getElementById("fromAnswer").style.display='block';
+
+}
+function revealEducation(){
+    document.getElementById("educationAnswer").style.display='block';
+
+}
+function revealProjects(){
+    document.getElementById("projectsAnswer").style.display='block';
+
+}
+function revealFun(){
+    document.getElementById("funAnswer").style.display='block';
+
+}
